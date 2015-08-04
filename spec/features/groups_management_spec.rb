79d1ce1a8948ecb6121d33c6e47f64c26a37b08f
Nell Shamrell <nellshamrell@gmail.com>
2015-08-04 14:29:07 -0700

    adding in specs - looking for a Create Group button

--- spec/features/groups_management_spec.rb
+++ spec/features/groups_management_spec.rb
@@ -7,8 +7,39 @@ feature 'groups management' do
     sign_in(user)
   end
 
-  scenario 'user visits their profile page' do
-    visit user_path(user)
-    expect(page).to have_link('Groups')
+  describe 'user visits their profile page' do
+    it 'shows the Groups link' do
+      visit user_path(user)
+      expect(page).to have_link('Groups')
+    end
+  end
+
+  describe 'user clicks the Groups link' do
+    before do
+      visit user_path(user)
+      click_link('Groups')
+    end
+
+    it 'shows the Create Group link' do
+      expect(page).to have_link('Create Group')
+    end
+
+    describe 'user clicks the Create Group link' do
+      it 'shows the new group form'
+        #expect(page).to have_field("Group Name")
+
+      context 'when the group info is not valid' do
+        it 'shows an error message'
+
+        it 'shows the new group form'
+      end
+
+      context 'when the group info is valid' do
+        it 'shows a success message'
+
+        it 'shows the group view'
+      end
+
+    end
   end
 end
