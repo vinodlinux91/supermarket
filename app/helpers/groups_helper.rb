ad821c8e7cfae5e1f73a6515ee2069fe0ba2238c
Nell Shamrell <nellshamrell@gmail.com>
2015-09-17 16:50:19 -0700

    refactoring view to use a helper method to determine if the current user is an admin member of a group

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,3 +1,5 @@
 module GroupsHelper
-
+  def admin_member?(user, group)
+    group.group_members.where(user_id: user.id, admin: true).present?
+  end
 end
--- spec/helpers/groups_helper_spec.rb
+++ spec/helpers/groups_helper_spec.rb
@@ -6,13 +6,25 @@ describe GroupsHelper do
     let(:user) { create(:user) }
 
     let!(:member) do
-      create(:group_member)
+      create(:group_member, user: user, group: group)
     end
 
     let(:admin_user) { create(:user) }
 
     let!(:admin_member) do
-      create(:admin_group_member)
+      create(:admin_group_member, user: admin_user, group: group)
+    end
+
+    context 'when then user is an admin member' do
+      it 'returns true' do
+        expect(helper.admin_member?(admin_user, group)).to eq(true)
+      end
+    end
+
+    context 'when the user is NOT an admin member' do
+      it 'returns false' do
+        expect(helper.admin_member?(user, group)).to eq(false)
+      end
     end
 
   end
