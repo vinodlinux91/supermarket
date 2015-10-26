809e4b01ecae800f0619d3b669e9dbd4f75923f0
Nell Shamrell <nellshamrell@gmail.com>
2015-10-26 16:57:31 -0700

    retroactively adds feature specs around adding a group of collaborators

--- spec/features/cookbook_collaboration_spec.rb
+++ spec/features/cookbook_collaboration_spec.rb
@@ -33,4 +33,29 @@ describe 'cookbook collaboration' do
     find('[rel*=remove-cookbook-collaborator]').trigger('click')
     expect(page).to have_no_css('div.gravatar-container')
   end
+
+  context 'adding groups of collaborators' do
+    let!(:group_member) { create(:group_member) }
+    let!(:group) { group_member.group }
+    let!(:group_member2) { create(:group_member, group: group) }
+
+    before do
+      sign_in(sally)
+      navigate_to_cookbook
+
+      expect(group.group_members).to include(group_member, group_member2)
+    end
+
+    it 'allows the owner to add a group of collaborators' do
+      find('#manage').click
+      find('[rel*=add-collaborator]').click
+      obj = find('.groups').set(group.id)
+
+      click_button('Add')
+
+
+      expect(page).to have_link("#{group_member.user.first_name} #{group_member.user.last_name}", href: user_path(group_member.user))
+      expect(page).to have_link("#{group_member2.user.first_name} #{group_member2.user.last_name}", href: user_path(group_member2.user))
+    end
+  end
 end
