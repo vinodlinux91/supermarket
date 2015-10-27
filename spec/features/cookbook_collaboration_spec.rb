96ec0ea19769be7feb5768666eef09087e23159b
Nell Shamrell <nellshamrell@gmail.com>
2015-10-27 15:20:11 -0700

    adds integration test around adding a group member as a collaborator to cookbooks a group is associated with

--- spec/features/cookbook_collaboration_spec.rb
+++ spec/features/cookbook_collaboration_spec.rb
@@ -35,7 +35,7 @@ describe 'cookbook collaboration' do
   end
 
   context 'adding groups of collaborators' do
-    let!(:group_member) { create(:group_member) }
+    let!(:group_member) { create(:group_member, user: sally) }
     let!(:group) { group_member.group }
     let!(:group_member2) { create(:group_member, group: group) }
 
@@ -56,6 +56,23 @@ describe 'cookbook collaboration' do
 
       expect(page).to have_link("#{group_member.user.first_name} #{group_member.user.last_name}", href: user_path(group_member.user))
       expect(page).to have_link("#{group_member2.user.first_name} #{group_member2.user.last_name}", href: user_path(group_member2.user))
+    end
+
+    context 'when a member is added to the group' do
+      let(:existing_user) { create(:user) }
+
+      before do
+        expect(page).to_not have_link("#{existing_user.first_name} #{existing_user.last_name}", href: user_path(existing_user))
+      end
+
+      it 'adds the member as a contributor to the cookbook' do
+        visit group_path(group)
+        click_link('Add Member')
+        fill_in('User ID', with: "#{existing_user.id}")
+        click_button('Add Member')
+        navigate_to_cookbook
+        expect(page).to have_link("#{existing_user.first_name} #{existing_user.last_name}", href: user_path(existing_user))
+      end
     end
   end
 end
